a64d5c0f185b0181fab79c9fec8246792ba9091b
Junxiujunxiu <132806576+Junxiujunxiu@users.noreply.github.com>
2024-08-29 11:44:02 +1200

    Update create-article.dto.ts
    added documentation

--- assignmentB/backend/src/api/articles/create-article.dto.ts
+++ assignmentB/backend/src/api/articles/create-article.dto.ts
@@ -1,5 +1,7 @@
 import { IsString, IsArray, IsNumber } from 'class-validator';
 
+//DTO is in application layer, often in controllers and servoce, to validate and transfer data between different parts of the application
+//when you receive a request to create a new article, you use this DTO to validate that the incoming data has the correct structure and types 
 export class CreateArticleDto {
   @IsString()
   readonly title: string;
