0fbc2a9721e85dc2feb29366ca5d032ba617185e
Junxiu Wu <sparky46465@gmail.com>
2024-10-22 02:00:22 +1300

    a
    a

--- assignmentB/frontend/src/pages/_app.tsx
+++ assignmentB/frontend/src/pages/_app.tsx
@@ -13,4 +13,4 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
             <Component {...pageProps} />
         </SessionProvider>);
 }
-export default MyApp;
+export default MyApp; 
