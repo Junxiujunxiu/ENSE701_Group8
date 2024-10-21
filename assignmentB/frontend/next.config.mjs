7c543f09448b26acde98a10f17aafbb472642050
Junxiu Wu <sparky46465@gmail.com>
2024-10-22 01:31:45 +1300

    exclude the test from building
    exclude the test from building

--- assignmentB/frontend/next.config.mjs
+++ assignmentB/frontend/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Ignore .test.js/.test.tsx files from the build process
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.test\.(js|ts|tsx)$/,
+        use: 'ignore-loader',
+      });
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
